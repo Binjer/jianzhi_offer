2df2599f5cd7e2e65c7a076873db24b5af501208
Binjer <binjren@163.com>
2019-08-01 23:41:16 +0800

    已完善.
    Signed-off-by: Binjer <binjren@163.com>

--- code/test26_prob27.py
+++ code/test26_prob27.py
@@ -1,17 +1,15 @@
-# 二叉树的镜像
+# 面试题27：二叉树的镜像
+
 
 # 递归解法
-class Solution(object):
+class Solution1(object):
     # 返回镜像树的根节点
     def Mirror(self, root):
         # write code here
         if not root:
-            return
+            return root
 
-        # 这个判断不是必须的吗? 应该判断一下比较合适
-        if root.left is None and root.right is None:
-            return
-
+        # 先前序遍历树的每个节点----根, 左, 右
         # 交换左右节点
         temp = root.left
         root.left = root.right
@@ -25,8 +23,11 @@ class Solution(object):
         if root.right:
             self.Mirror(root.right)
 
+        return root
 
-# 循环解法, 借助栈或者双向队列完成后进先出的功能
+
+# 循环解法
+# 利用栈来完成
 class Solution2(object):
     # 返回镜像树的根节点
     def Mirror(self, root):
@@ -34,18 +35,20 @@ class Solution2(object):
         if not root:
             return
 
-        q = []
-        q.append(root)
+        stack1 = []
+        stack1.append(root)
 
-        while len(q) > 0:
-            par = q.pop()
+        while len(stack1) > 0:
+            par = stack1.pop()
 
             temp = par.left
             par.left = par.right
             par.right = temp
 
             if par.left:
-                q.append(par.left)
+                stack1.append(par.left)
 
             if par.right:
-                q.append(par.right)
+                stack1.append(par.right)
+
+        return root
--- code/test27_prob28.py
+++ code/test27_prob28.py
@@ -1,26 +1,28 @@
-# 对称的二叉树
+# 面试题28：对称的二叉树
+
 # 思路分析:
 # 常规的前中后序遍历都是先遍历左节点再遍历右节点
 # 如果我们能定义一种先遍历右节点, 再遍历左节点的遍历方式, 并与前面的遍历结果比较.
 # 如果两者一样, 说明二叉树是对称的
 
+
+# 镜像的前序遍历就是原树的后向遍历
 class Solution(object):
     def isSymmetrical(self, pRoot):
-        # write code here
+        if not pRoot:
+            return True  # 空树要按照题目要求来
 
         return self.isSymmert(pRoot, pRoot)
 
     def isSymmert(self, root1, root2):
-        cur1 = root1
-        cur2 = root2
 
-        if cur1 is None and cur2 is None:
+        if root1 is None and root2 is None:  # 把空指针也考虑进来
             return True
 
-        if cur1 is None or cur2 is None:
+        if root1 is None or root2 is None:  # 左右一个为空一个不为空则返回False
             return False
 
-        if cur1.val != cur2.val:
+        if root1.val != root2.val:  # 左右值不一致则返回False
             return False
 
-        return self.isSymmert(cur1.left, cur2.right) and self.isSymmert(cur1.right, cur2.left)
+        return self.isSymmert(root1.left, root2.right) and self.isSymmert(root1.right, root2.left)
